6f9052101a61a2ad13f1974d6c9df0270145b2ac
Emmanuel Mota <emmanuelmota@gmail.com>
2020-10-24 11:34:12 -0700

    Install newrelic

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
+require('newrelic');
 const express = require('express');
 const db = require('../database/index.js');
+
 
 const app = express();
 
@@ -11,7 +13,7 @@ app.post('/api/room/calendar', function (req, res) {
 // app.get('/api/room/calendar', (req, res) => {
 //   db.get('2', req.query.month, (err, result) => res.status(200).send(result));
 //   // console.log('been invoked');
-});
+//});
 app.get('/api/room/:roomId/calendar/:month', (req, res) => {
   const { roomId, month } = req.params;
   db.get(roomId, month, (err, result) => res.status(200).send(result));
