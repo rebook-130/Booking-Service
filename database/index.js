761e44044724ee580294bcaf999733fab04c0ac5
Emmanuel Mota <emmanuelmota@gmail.com>
2020-10-24 21:53:39 -0700

    GET reservation by roomID route

--- database/index.js
+++ database/index.js
@@ -1,65 +1,40 @@
-const mongoose = require('mongoose');
+const { Client } = require('pg');
+const config = { database: 'rebook' };
+const client = new Client(config);
 
-// Uncomment for localhost
-mongoose.connect('mongodb://localhost/calendar');
-// Uncomment for Proxy use
-// mongoose.connect('mongodb://database/calendar');
-
-const calendarSchema = mongoose.Schema({
-  roomId: String,
-  date: String,
-  month: Number,
-  day: Number,
-  price: Number,
-  maxGuest: Number,
-  cleaningFee: Number,
-  taxes: Number,
-  rating: Number,
-  booked: Boolean,
+client.connect(err => {
+  if (err) {
+    console.error('connection error', err.stack);
+  } else {
+    console.log('connected!');
+  }
 });
 
-const Calendar = mongoose.model('Calendar', calendarSchema);
-
-const save = (singleDate) => {
-  // console.log(repos);
-  // console.log(repos[0].name, repos[0].owner.login, repos[0].url, repos[0].watchers_count, repos[0].forks_count);
-
-  const add = new Calendar({
-    roomId: singleDate.roomId,
-    date: singleDate.date,
-    month: singleDate.month,
-    day: singleDate.day,
-    price: singleDate.price,
-    maxGuest: singleDate.maxGuest,
-    cleaningFee: singleDate.cleaningFee,
-    taxes: singleDate.taxes,
-    rating: singleDate.rating,
-    booked: singleDate.booked,
-  });
-
-  add.save((err) => {
-    console.log(err);
-    // Calendar.deleteMany({}, (err)=>{console.log(err)});  // <- here the wa to clean the data base
-    // Calendar.find({}).exec((err,result)=>{ console.log(result) })
+const getRoomReservations = (roomId, cb) => {
+  // Get all reservations from a room
+  var sql = `SELECT * FROM reservations WHERE room_id=${roomId}`;
+  client.query(sql, (err, res) => {
+    if (err) {
+      cb(err);
+    } else {
+      cb(null, res);
+    }
   });
 };
 
-const postThis = (document) => {
-  Calendar.insert(document);
+const createReservation = (params, callback) => {
+  // Create a review for a business from a user
+  var sql = 'INSERT INTO reviews (id, business_id, users_id, stars, date, content, useful, funny, cool) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
+  client.query(sql, params, (err, data) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, data);
+    }
+  });
 };
 
-const get = (roomId, month, cb) => {
-  // How should I get the month before and the month after ?
-  // I need to get 3 months worth of data
-  Calendar.find({ 'roomId': roomId, 'month': month}).exec((err, result) => { cb(err, result); });
-  // Calendar.find({ 'roomId': roomId, 'date': date}).exec((err, result) => { cb(err, result); });
+module.exports = {
+  client,
+  getRoomReservations,
 };
-
-const deleteAll = () => {
-  Calendar.deleteMany({}, (err) => { console.log(err); }); // <- here the wa to clean the data base
-};
-
-module.exports.save = save;
-module.exports.get = get;
-module.exports.delete = deleteAll;
-module.exports.postThis = postThis;
--- server/index.js
+++ server/index.js
@@ -1,24 +1,38 @@
 require('newrelic');
 const express = require('express');
+const bodyParser = require('body-parser')
 const db = require('../database/index.js');
-
 
 const app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
+app.use(bodyParser.json());
 
-app.post('/api/room/calendar', function (req, res) {
+// app.post('/api/room/calendar', function (req, res) {
+// });
+
+app.get('/api/room/:roomId/calendar/', (req, res) => {
+  const { roomId } = req.params;
+  console.log(`${roomId}`);
+
+  db.getRoomReservations(roomId, (err, result) => {
+    if (err) {
+      res.status(400).send(err);
+    }
+    res.status(200).send(result.rows);
+  });
 });
 
 // app.get('/api/room/calendar', (req, res) => {
+//   console.log('app.get was invoked');
 //   db.get('2', req.query.month, (err, result) => res.status(200).send(result));
+
+// });
+// app.get('/api/room/:roomId/calendar/:month', (req, res) => {
+//   const { roomId, month } = req.params;
+//   db.get(roomId, month, (err, result) => res.status(200).send(result));
 //   // console.log('been invoked');
-//});
-app.get('/api/room/:roomId/calendar/:month', (req, res) => {
-  const { roomId, month } = req.params;
-  db.get(roomId, month, (err, result) => res.status(200).send(result));
-  // console.log('been invoked');
-});
+// });
 
 const port = 3002;
 
